f9fe70bde92db19332db83c34a0972465823c571
yura <yura@DESKTOP-DHD4061>
2024-03-30 21:55:16 +0300

    Добавьте файлы проекта.

---
+++ WpfAppName/Helper/FindPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppName.Model;
+
+namespace WpfAppName.Helper
+{
+    internal class FindPerson
+    {
+        int id;
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+        public bool PersonPredicate(Person person)
+        {
+            return person.Id == id;
+        }
+    }
+}
